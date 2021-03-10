f7d46139de23b1f96841381565492af127827171
Tibor Rozsa <rozsa.tibor96@gmail.com>
2021-03-10 13:48:16 +0100

    All the unfinished things and stuff and things

--- devtools/visualization_demo.py
+++ devtools/visualization_demo.py
@@ -1,9 +1,10 @@
 import mozaik.stimuli.vision.topographica_based as topo
 import mozaik.experiments.vision as exp
+import mozaik.experiments.apparent_motion as am
 import stimulus_visualization as viz
 import dummy_model as dm
 from parameters import ParameterSet
-
+import numpy as np
 
 def experiment_default_parameters():
     return {
@@ -130,6 +131,27 @@ def MapTwoStrokeGabor_default_parameters():
     return d
 
 
+def CompareSlowVersusFastGaborMotion_default_parameters():
+    d = {
+            "num_trials": 2,
+            "x": 1,
+            "y": 1,
+            "orientation": 0,
+            "phase": 1,
+            "spatial_frequency": 2,
+            "sigma": 0.17,
+            "n_sigmas": 3.0,
+            "center_relative_luminance": 0.5,
+            "surround_relative_luminance": 0.7,
+            "movement_speeds": [5.0, 180.0],
+            #"angles": list(np.linspace(0, 2 * np.pi, 12, endpoint=False)),
+            "angles": [0],
+            "moving_gabor_orientation_radial": True,
+            "radius": 5,
+        }
+    return d
+
+
 def demo_show_frame():
     params = ContinuousGaborMovementAndJump_default_parameters()
     stim = topo.ContinuousGaborMovementAndJump(**params)
@@ -178,7 +200,7 @@ def demo_experiment_0():
     model = dm.DummyModel(**visual_stim_default_parameters())
     params = MapTwoStrokeGabor_default_parameters()
     parameters = ParameterSet(params)
-    experiment = exp.MapTwoStrokeGabor(model=model, parameters=parameters)
+    experiment = am.MapTwoStrokeGabor(model=model, parameters=parameters)
     viz.show_experiment(experiment, merge_stimuli=True)
 
 
@@ -186,13 +208,24 @@ def demo_experiment_1():
     model = dm.DummyModel(**visual_stim_default_parameters())
     params = MapSimpleGabor_default_parameters()
     parameters = ParameterSet(params)
-    experiment = exp.MapSimpleGabor(model=model, parameters=parameters)
+    experiment = am.MapSimpleGabor(model=model, parameters=parameters)
     viz.show_experiment(experiment, merge_stimuli=False, grid=True)
 
 
-def main():
+def demo_experiment_2():
+    model_params = visual_stim_default_parameters()
+    model_params["frame_duration"] = 2
+    model = dm.DummyModel(**model_params)
+    params = CompareSlowVersusFastGaborMotion_default_parameters()
+    parameters = ParameterSet(params)
+    experiment = am.CompareSlowVersusFastGaborMotion(model=model, parameters=parameters)
+    viz.show_experiment(experiment, merge_stimuli=False)
 
-    if True:
+
+def main():
+    demo_experiment_2()
+
+    if False:
         # Try out show_stimulus arguments
         demo_stimulus_0()
         demo_stimulus_1()
@@ -201,12 +234,12 @@ def main():
         demo_stimulus_0_frame_delay()
         # demo_stimulus_0_animate() # Commented out as you'd have to click through all frames
 
-    if True:
+    if False:
         # Try out visualizing experiments
         demo_experiment_0()
         demo_experiment_1()
 
-    if True:
+    if False:
         # Try out the underlying show_frame function
         demo_show_frame()
 
